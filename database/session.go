9df94c1de2b09786e4b4642d2d13c69e366e053c
Lekuru <contact@lekuru.xyz>
2026-02-11 14:24:29 +0100

    Add gorm session creator

--- database/session.go
+++ database/session.go
@@ -1,1 +1,38 @@
 package database
+
+import (
+	"time"
+
+	"github.com/osuTitanic/common-go/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// CreateSession opens a postgres connection using values from `config.Config`
+func CreateSession(cfg *config.Config) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(cfg.PostgresDSN()), &gorm.Config{
+		Logger: NewGormLogger(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	if cfg.PostgresPoolEnabled {
+		// Use configured pool values
+		sqlDB.SetMaxOpenConns(cfg.PostgresPoolSizeOverflow)
+		sqlDB.SetMaxIdleConns(cfg.PostgresPoolSize)
+		sqlDB.SetConnMaxLifetime(time.Duration(cfg.PostgresPoolRecycle) * time.Second)
+	}
+
+	if cfg.PostgresPoolPrePing {
+		if err := sqlDB.Ping(); err != nil {
+			return nil, err
+		}
+	}
+	return db, nil
+}
