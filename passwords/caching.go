71c88dd7d43f13f88a9a79f361220d2b5bb6da1c
Lekuru <contact@lekuru.xyz>
2026-03-02 19:07:28 +0100

    Add todo for password caching max size

--- passwords/caching.go
+++ passwords/caching.go
@@ -1,5 +1,8 @@
 package passwords
 
+// TODO: Make sure this doesn't cause memory issues in the future
+//       There should be some sort of max size, but this will do (for now *cough*)
+
 var passwordCache = map[string]bool{}
 
 func GetPasswordCache() map[string]bool {
