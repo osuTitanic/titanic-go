a8a07bf5e6e79dbe9ee4e18659e1330d985ab9c4
Lekuru <contact@lekuru.xyz>
2026-04-04 21:50:23 +0200

    Add forum icon enum

---
+++ internal/constants/forums.go
@@ -0,0 +1,20 @@
+package constants
+
+type ForumIcon int
+
+const (
+	ForumIconHeart       ForumIcon = 1
+	ForumIconHeartPop    ForumIcon = 2
+	ForumIconBubble      ForumIcon = 3
+	ForumIconBubblePop   ForumIcon = 4
+	ForumIconFire        ForumIcon = 5
+	ForumIconStar        ForumIcon = 6
+	ForumIconRadioactive ForumIcon = 7
+	ForumIconAlert       ForumIcon = 8
+	ForumIconInfo        ForumIcon = 9
+	ForumIconQuestion    ForumIcon = 10
+	ForumIconOsu         ForumIcon = 11
+	ForumIconTaiko       ForumIcon = 12
+	ForumIconCatch       ForumIcon = 13
+	ForumIconMania       ForumIcon = 14
+)
--- internal/schemas/forums.go
+++ internal/schemas/forums.go
@@ -1,6 +1,10 @@
 package schemas
 
-import "time"
+import (
+	"time"
+
+	"github.com/osuTitanic/titanic-go/internal/constants"
+)
 
 type Forum struct {
 	Id          int       `gorm:"column:id;primaryKey;autoIncrement"`
@@ -24,22 +28,22 @@ func (Forum) TableName() string {
 }
 
 type ForumTopic struct {
-	Id            int        `gorm:"column:id;primaryKey;autoIncrement"`
-	ForumId       int        `gorm:"column:forum_id"`
-	CreatorId     int        `gorm:"column:creator_id"`
-	Title         string     `gorm:"column:title"`
-	StatusText    *string    `gorm:"column:status_text"`
-	CreatedAt     time.Time  `gorm:"column:created_at;autoCreateTime"`
-	LastPostAt    time.Time  `gorm:"column:last_post_at;autoCreateTime"`
-	LockedAt      *time.Time `gorm:"column:locked_at"`
-	Views         int        `gorm:"column:views;default:0"`
-	PostCount     int        `gorm:"column:post_count;default:0;->"`
-	IconId        *int       `gorm:"column:icon"`
-	CanChangeIcon bool       `gorm:"column:can_change_icon;default:true"`
-	CanStar       bool       `gorm:"column:can_star;default:false"`
-	Announcement  bool       `gorm:"column:announcement;default:false"`
-	Hidden        bool       `gorm:"column:hidden;default:false"`
-	Pinned        bool       `gorm:"column:pinned;default:false"`
+	Id            int                  `gorm:"column:id;primaryKey;autoIncrement"`
+	ForumId       int                  `gorm:"column:forum_id"`
+	CreatorId     int                  `gorm:"column:creator_id"`
+	Title         string               `gorm:"column:title"`
+	StatusText    *string              `gorm:"column:status_text"`
+	CreatedAt     time.Time            `gorm:"column:created_at;autoCreateTime"`
+	LastPostAt    time.Time            `gorm:"column:last_post_at;autoCreateTime"`
+	LockedAt      *time.Time           `gorm:"column:locked_at"`
+	Views         int                  `gorm:"column:views;default:0"`
+	PostCount     int                  `gorm:"column:post_count;default:0;->"`
+	IconId        *constants.ForumIcon `gorm:"column:icon"`
+	CanChangeIcon bool                 `gorm:"column:can_change_icon;default:true"`
+	CanStar       bool                 `gorm:"column:can_star;default:false"`
+	Announcement  bool                 `gorm:"column:announcement;default:false"`
+	Hidden        bool                 `gorm:"column:hidden;default:false"`
+	Pinned        bool                 `gorm:"column:pinned;default:false"`
 
 	Forum       *Forum            `gorm:"foreignKey:ForumId;references:Id"`
 	Icon        *ForumIcon        `gorm:"foreignKey:IconId;references:Id"`
@@ -55,19 +59,19 @@ func (ForumTopic) TableName() string {
 }
 
 type ForumPost struct {
-	Id         int64     `gorm:"column:id;primaryKey;autoIncrement"`
-	TopicId    int       `gorm:"column:topic_id"`
-	ForumId    int       `gorm:"column:forum_id"`
-	UserId     int       `gorm:"column:user_id"`
-	IconId     *int      `gorm:"column:icon_id"`
-	Content    string    `gorm:"column:content"`
-	CreatedAt  time.Time `gorm:"column:created_at;autoCreateTime"`
-	EditTime   time.Time `gorm:"column:edit_time;autoCreateTime"`
-	EditCount  int       `gorm:"column:edit_count;default:0"`
-	EditLocked bool      `gorm:"column:edit_locked;default:false"`
-	Hidden     bool      `gorm:"column:hidden;default:false"`
-	Draft      bool      `gorm:"column:draft;default:false"`
-	Deleted    bool      `gorm:"column:deleted;default:false"`
+	Id         int64                `gorm:"column:id;primaryKey;autoIncrement"`
+	TopicId    int                  `gorm:"column:topic_id"`
+	ForumId    int                  `gorm:"column:forum_id"`
+	UserId     int                  `gorm:"column:user_id"`
+	IconId     *constants.ForumIcon `gorm:"column:icon_id"`
+	Content    string               `gorm:"column:content"`
+	CreatedAt  time.Time            `gorm:"column:created_at;autoCreateTime"`
+	EditTime   time.Time            `gorm:"column:edit_time;autoCreateTime"`
+	EditCount  int                  `gorm:"column:edit_count;default:0"`
+	EditLocked bool                 `gorm:"column:edit_locked;default:false"`
+	Hidden     bool                 `gorm:"column:hidden;default:false"`
+	Draft      bool                 `gorm:"column:draft;default:false"`
+	Deleted    bool                 `gorm:"column:deleted;default:false"`
 
 	Modding []BeatmapModding `gorm:"foreignKey:PostId;references:Id"`
 	User    *User            `gorm:"foreignKey:UserId;references:Id"`
@@ -81,10 +85,10 @@ func (ForumPost) TableName() string {
 }
 
 type ForumIcon struct {
-	Id       int    `gorm:"column:id;primaryKey;autoIncrement"`
-	Name     string `gorm:"column:name"`
-	Location string `gorm:"column:location"`
-	Order    int    `gorm:"column:order;default:0"`
+	Id       constants.ForumIcon `gorm:"column:id;primaryKey;autoIncrement"`
+	Name     string              `gorm:"column:name"`
+	Location string              `gorm:"column:location"`
+	Order    int                 `gorm:"column:order;default:0"`
 
 	Topics []ForumTopic `gorm:"foreignKey:IconId;references:Id"`
 	Posts  []ForumPost  `gorm:"foreignKey:IconId;references:Id"`
