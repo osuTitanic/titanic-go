cd1a5cd7ff6e5df3ff6b2b2d1465bf8cca796892
Lekuru <contact@lekuru.xyz>
2026-04-10 17:37:24 +0200

    Add todo's for new tasks

---
+++ services/jobs/internal/tasks/users_autodelete.go
@@ -0,0 +1,5 @@
+package tasks
+
+// TODO: Add task that auto-deletes users that never activated their account 7 days after registration.
+//       "Delete" in this case means to rename their account to `DeletedUser<ID>`, changing the email to
+// 		 `deleteduser<ID>@<domain>` & clearing their verification entries.
