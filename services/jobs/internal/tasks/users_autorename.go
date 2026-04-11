eb03d4145908aba1d9403a277c085b850fac958e
Lekuru <contact@lekuru.xyz>
2026-04-11 19:19:49 +0200

    Remove to-do autorename task

--- services/jobs/internal/tasks/users_autorename.go
+++
@@ -1,4 +0,0 @@
-package tasks
-
-// TODO: Add task that auto-renames users to `<username>_old` once they have been inactive for 1 year.
-//       Certain users may be exempt from this, which would require a special flag in the user schema.
